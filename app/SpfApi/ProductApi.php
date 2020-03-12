7ddabf18dfef673ff6384899178d22c55ef264df
Vu Le <leewanvu@gmail.com>
2020-03-12 14:18:41 +0700

    Add comment

--- app/SpfApi/ProductApi.php
+++ app/SpfApi/ProductApi.php
@@ -7,7 +7,14 @@ use Exception;
 
 class ProductApi extends BaseApi
 {
-    public function getProducts(string $shop, string $token)
+    /**
+     * Get products
+     *
+     * @param string $shop
+     * @param string $token
+     * @return array
+     */
+    public function getProducts(string $shop, string $token) : array
     {
         try{
             $response = $this->client->request('GET', "https://{$shop}.myshopify.com/admin/api/{$this->apiVersion}/products.json",
